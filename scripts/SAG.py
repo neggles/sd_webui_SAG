a5e51318a45592a3bbf891766ae8fee0e5866b71
Andrew Powers-Holmes <aholmes@omnom.net>
2023-04-24 11:19:28 +1000

    SAG: refactor to remove all but one global var

--- scripts/SAG.py
+++ scripts/SAG.py
@@ -1,6 +1,7 @@
 import math
 import os
 from inspect import isfunction
+from typing import Tuple
 
 import gradio as gr
 import torch
@@ -22,6 +23,8 @@ from modules.script_callbacks import (
 from scripts import xyz_grid_support_sag
 
 _ATTN_PRECISION = os.environ.get("ATTN_PRECISION", "fp32")
+_DEFAULT_SCALE = 0.75
+_DEFAULT_MASK_THRESH = 1.0
 
 
 def exists(val):
@@ -87,6 +90,8 @@ class LoggedSelfAttention(nn.Module):
 
 
 def xattn_forward_log(self, x, context=None, mask=None):
+    global current_selfattn_map
+
     h = self.heads
 
     q = self.to_q(x)
@@ -95,7 +100,6 @@ def xattn_forward_log(self, x, context=None, mask=None):
     v = self.to_v(context)
 
     q, k, v = map(lambda t: rearrange(t, "b n (h d) -> (b h) n d", h=h), (q, k, v))
-
     # force cast to fp32 to avoid overflowing
     if _ATTN_PRECISION == "fp32":
         with torch.autocast(enabled=False, device_type="cuda"):
@@ -103,7 +107,6 @@ def xattn_forward_log(self, x, context=None, mask=None):
             sim = einsum("b i d, b j d -> b i j", q, k) * self.scale
     else:
         sim = einsum("b i d, b j d -> b i j", q, k) * self.scale
-
     del q, k
 
     if exists(mask):
@@ -116,30 +119,15 @@ def xattn_forward_log(self, x, context=None, mask=None):
     sim = sim.softmax(dim=-1)
 
     self.attn_probs = sim
-    global current_selfattn_map
     current_selfattn_map = sim
 
     out = einsum("b i j, b j d -> b i d", sim, v)
     out = rearrange(out, "(b h) n d -> b n (h d)", h=h)
     out = self.to_out(out)
-    global current_outsize
-    current_outsize = out.shape[-2:]
     return out
 
 
-saved_original_selfattn_forward = None
-current_selfattn_map = None
-current_sag_guidance_scale = 1.0
-sag_enabled = False
-sag_mask_threshold = 1.0
-
-current_xin = None
-current_outsize = (64, 64)
-current_batch_size = 1
-current_degraded_pred = None
-current_unet_kwargs = {}
-current_uncond_pred = None
-current_degraded_pred_compensation = None
+current_selfattn_map = []
 
 
 def gaussian_blur_2d(img, kernel_size, sigma):
@@ -164,48 +152,59 @@ def gaussian_blur_2d(img, kernel_size, sigma):
 
 
 class Script(scripts.Script):
+    enabled: bool = False
+    guidance_scale: float = _DEFAULT_SCALE
+    mask_threshold: float = _DEFAULT_MASK_THRESH
+    callbacks_added: bool = False
+
+    original_selfattn_forward: callable = None
+    xin = None
+    batch_size: int = 1
+    degraded_pred = None
+    degraded_pred_compensation = None
+    uncond_pred = None
+    unet_kwargs = {}
+
     def __init__(self):
         pass
 
     def title(self):
         return "Self Attention Guidance"
 
-    def show(self, is_img2img):
+    def show(self, is_img2img: bool):
         return scripts.AlwaysVisible
 
-    def denoiser_callback(self, parms: CFGDenoiserParams):
-        if not sag_enabled:
+    def denoiser_callback(self, params: CFGDenoiserParams):
+        if self.enabled is not True:
             return
-        global current_xin, current_batch_size
 
         # logging current uncond size for cond/uncond output separation
-        current_batch_size = parms.text_uncond.shape[0]
+        self.batch_size = params.text_uncond.shape[0]
         # logging current input for eps calculation later
-        current_xin = parms.x[-current_batch_size:]
+        self.xin = params.x[-(self.batch_size) :]
 
         # logging necessary information for SAG pred
-        current_uncond_emb = parms.text_uncond
-        current_sigma = parms.sigma
-        current_image_cond_in = parms.image_cond
-        global current_unet_kwargs
-        current_unet_kwargs = {
-            "sigma": current_sigma[-current_batch_size:],
-            "image_cond": current_image_cond_in[-current_batch_size:],
+        current_uncond_emb = params.text_uncond
+        current_sigma = params.sigma
+        current_image_cond_in = params.image_cond
+        self.unet_kwargs = {
+            "sigma": current_sigma[-(self.batch_size) :],
+            "image_cond": current_image_cond_in[-(self.batch_size) :],
             "text_uncond": current_uncond_emb,
         }
 
     def denoised_callback(self, params: CFGDenoisedParams):
-        if not sag_enabled:
+        if self.enabled is not True:
             return
+        global current_selfattn_map
         # output from DiscreteEpsDDPMDenoiser is already pred_x0
-        uncond_output = params.x[-current_batch_size:]
+        uncond_output = params.x[-(self.batch_size) :]
         original_latents = uncond_output
-        global current_uncond_pred
-        current_uncond_pred = uncond_output
+        self.uncond_pred = uncond_output
 
         # Produce attention mask
-        # We're only interested in the last current_batch_size*head_count slices of logged self-attention map
-        attn_map = current_selfattn_map[-current_batch_size * 8 :]
+        # We're only interested in the last (self.batch_size)*head_count slices of logged self-attention map
+        attn_map = current_selfattn_map[-(self.batch_size * 8) :]
         bh, hw1, hw2 = attn_map.shape
         b, latent_channel, latent_h, latent_w = original_latents.shape
         h = 8
@@ -213,7 +212,7 @@ class Script(scripts.Script):
         middle_layer_latent_size = [math.ceil(latent_h / 8), math.ceil(latent_w / 8)]
 
         attn_map = attn_map.reshape(b, h, hw1, hw2)
-        attn_mask = attn_map.mean(1, keepdim=False).sum(1, keepdim=False) > sag_mask_threshold
+        attn_mask = attn_map.mean(1, keepdim=False).sum(1, keepdim=False) > self.mask_threshold
         attn_mask = (
             attn_mask.reshape(b, middle_layer_latent_size[0], middle_layer_latent_size[1])
             .unsqueeze(1)
@@ -225,75 +224,74 @@ class Script(scripts.Script):
         # Blur according to the self-attention mask
         degraded_latents = gaussian_blur_2d(original_latents, kernel_size=9, sigma=1.0)
         degraded_latents = degraded_latents * attn_mask + original_latents * (1 - attn_mask)
+        renoised_degraded_latent = degraded_latents - (uncond_output - self.xin)
 
-        renoised_degraded_latent = degraded_latents - (uncond_output - current_xin)
-        # renoised_degraded_latent = degraded_latents
         # get predicted x0 in degraded direction
-        global current_degraded_pred_compensation
-        current_degraded_pred_compensation = uncond_output - degraded_latents
+        self.degraded_pred_compensation = uncond_output - degraded_latents
         if shared.sd_model.model.conditioning_key == "crossattn-adm":
             condition_dict = {
-                "c_crossattn": current_unet_kwargs["text_uncond"],
-                "c_adm": current_unet_kwargs["image_cond"],
+                "c_crossattn": self.unet_kwargs["text_uncond"],
+                "c_adm": self.unet_kwargs["image_cond"],
             }
         else:
             condition_dict = {
-                "c_crossattn": current_unet_kwargs["text_uncond"],
-                "c_concat": [current_unet_kwargs["image_cond"]],
+                "c_crossattn": self.unet_kwargs["text_uncond"],
+                "c_concat": [self.unet_kwargs["image_cond"]],
             }
-        degraded_pred = params.inner_model(
+
+        self.degraded_pred = params.inner_model(
             renoised_degraded_latent,
-            current_unet_kwargs["sigma"],
+            self.unet_kwargs["sigma"],
             cond=condition_dict,
         )
-        global current_degraded_pred
-        current_degraded_pred = degraded_pred
 
     def cfg_after_cfg_callback(self, params: AfterCFGCallbackParams):
-        if not sag_enabled:
+        if self.enabled is not True:
             return
 
-        params.x = params.x + (
-            current_uncond_pred - (current_degraded_pred + current_degraded_pred_compensation)
-        ) * float(current_sag_guidance_scale)
+        params.x += (
+            self.uncond_pred - (self.degraded_pred + self.degraded_pred_compensation)
+        ) * self.guidance_scale
         params.output_altered = True
 
     def ui(self, is_img2img):
-        with gr.Accordion("Self Attention Guidance", open=False):
-            enabled = gr.Checkbox(label="Enabled", default=False)
-            scale = gr.Slider(label="Scale", minimum=-2.0, maximum=10.0, step=0.01, value=0.75)
-            mask_threshold = gr.Slider(
-                label="SAG Mask Threshold", minimum=0.0, maximum=2.0, step=0.01, value=1.0
+        with gr.Accordion("Self Attention Guidance", open=False, elem_id="sag_accordion"):
+            enabled = gr.Checkbox(label="Enabled", default=False, elem_id="sag_enabled")
+            scale = gr.Slider(
+                label="Scale", minimum=-2.0, maximum=10.0, step=0.01, value=0.75, elem_id="sag_scale"
             )
-
+            mask_threshold = gr.Slider(
+                label="Mask Threshold", minimum=0.0, maximum=2.0, step=0.01, value=1.0, elem_id="sag_mask_th"
+            )
         return [enabled, scale, mask_threshold]
 
-    def process(self, p: StableDiffusionProcessing, *args, **kwargs):
-        enabled, scale, mask_threshold = args
-        global sag_enabled, sag_mask_threshold
-        if enabled:
-            sag_enabled = True
-            sag_mask_threshold = mask_threshold
-            global current_sag_guidance_scale
-            current_sag_guidance_scale = scale
-            global saved_original_selfattn_forward
-            # replace target self attention module in unet with ours
+    def process(
+        self, p: StableDiffusionProcessing, enabled: bool, scale: float, mask_threshold: float, **kwargs
+    ):
+        if enabled is True:
+            self.enabled = enabled
+            self.mask_threshold = mask_threshold
+            self.guidance_scale = scale
 
+            # save original forward function for later restoration
             org_attn_module = (
                 shared.sd_model.model.diffusion_model.middle_block._modules["1"]
                 .transformer_blocks._modules["0"]
                 .attn1
             )
-            saved_original_selfattn_forward = org_attn_module.forward
+            self.original_selfattn_forward = org_attn_module.forward
+
+            # replace target self attention module in unet with ours
             org_attn_module.forward = xattn_forward_log.__get__(org_attn_module, org_attn_module.__class__)
 
-            p.extra_generation_params["SAG Guidance Scale"] = scale
-            p.extra_generation_params["SAG Mask Threshold"] = mask_threshold
+            # add extra parameters to the generation metadata
+            p.extra_generation_params["SAG Guidance Scale"] = self.guidance_scale
+            p.extra_generation_params["SAG Mask Threshold"] = self.mask_threshold
 
         else:
-            sag_enabled = False
+            self.enabled = False
 
-        if not hasattr(self, "callbacks_added"):
+        if self.callbacks_added is not True:
             on_cfg_denoiser(self.denoiser_callback)
             on_cfg_denoised(self.denoised_callback)
             on_cfg_after_cfg(self.cfg_after_cfg_callback)
@@ -301,17 +299,29 @@ class Script(scripts.Script):
 
         return
 
-    def postprocess(self, p, processed, *args):
-        enabled, scale, sag_mask_threshold = args
-        if enabled:
+    def postprocess(self, p, processed, enabled: bool, scale: float, mask_threshold: float):
+        if enabled is True:
             # restore original self attention module forward function
             attn_module = (
                 shared.sd_model.model.diffusion_model.middle_block._modules["1"]
                 .transformer_blocks._modules["0"]
                 .attn1
             )
-            attn_module.forward = saved_original_selfattn_forward
+            attn_module.forward = self.original_selfattn_forward
+            # Reset module storage state
+            self.clear_state()
         return
+
+    def clear_state(self):
+        global current_selfattn_map
+        current_selfattn_map = []
+        self.original_selfattn_forward = None
+        self.xin = None
+        self.batch_size = 1
+        self.degraded_pred = None
+        self.degraded_pred_compensation = None
+        self.uncond_pred = None
+        self.unet_kwargs = {}
 
 
 xyz_grid_support_sag.initialize(Script)
